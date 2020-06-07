e7ebdf37c8a9bce37f75fdf0ff415c3958167ba2
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-07 23:07:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-#def empty_board
-  #puts "   |   |   "
-  #puts "-----------"
-  #puts "   |   |   "
-  #puts "-----------"
-  #puts "   |   |   "
-#end
-
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
